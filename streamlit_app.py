c1f6702ebd4020aa81c4d1d1ee11879bb3fab7e4
Matthew Gilmore <117459396+mattxp9k@users.noreply.github.com>
2022-11-28 11:52:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.title('Build Your Own Fruit Smoothie)
+streamlit.title('Build Your Own Fruit Smoothie')
 streamlit.dataframe(my_fruit_list)
